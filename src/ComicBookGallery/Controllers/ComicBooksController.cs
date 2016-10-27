bc90fdcd39ed67348f69a2ecf8693893de3cf5e0
Dan <Dan@DESKTOP-HNOEPHQ>
2016-10-27 16:45:05 -0400

    added detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            "Hello World from the comic books controller";
+        }
     }
 }
